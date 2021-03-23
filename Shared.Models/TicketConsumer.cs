85658637620430250a871b7b97fa9237de228924
PaulNorthern <sculltakers111@gmail.com>
2021-03-23 13:59:03 +0300

    Добавьте файлы проекта.

---
+++ Shared.Models/TicketConsumer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Shared.Models
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
---
+++ Ticketing.Microservice/Controllers/TicketController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Shared.Models;
+
+namespace Ticketing.Microservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                //  name our Queue as ticketQueue
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                //  an endpoint to which we can send the shared model object.
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                //  push the message to the queue
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
